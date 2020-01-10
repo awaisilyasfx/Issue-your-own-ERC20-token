5d7ba575b08c651e9b033fa20be47f740dace69e
awaisilyasfx <59727792+awaisilyasfx@users.noreply.github.com>
2020-01-10 14:49:58 +0500

    recyclepcoin

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.24;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// 'recyclepcoin' token contract
 //
-// Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
+// Deployed to : 0xA628efc1055Ea35f14c1ab7b16137296d3d5D1F1
+// Symbol      : rpcoin
+// Name        : recyclepcoin Token
 // Total supply: 100000000
-// Decimals    : 18
+// Decimals    : 8
 //
 // Enjoy.
 //
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract FucksToken is ERC20Interface, Owned, SafeMath {
+contract recyclepcoin is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -113,12 +113,12 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
     // Constructor
     // ------------------------------------------------------------------------
     constructor() public {
-        symbol = "0FUCKS";
-        name = "0 Fucks Token";
-        decimals = 18;
-        _totalSupply = 100000000000000000000000000;
-        balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
-        emit Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+        symbol = "rpcoin";
+        name = "recyclepcoin Token";
+        decimals = 8;
+        _totalSupply = 100000000000000;
+        balances[0xA628efc1055Ea35f14c1ab7b16137296d3d5D1F1] = _totalSupply;
+        emit Transfer(address(0), 0xA628efc1055Ea35f14c1ab7b16137296d3d5D1F1, _totalSupply);
     }
 
 
